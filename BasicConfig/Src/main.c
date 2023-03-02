07001edfac9c29c7d36e7bef15425e02f1d1e92f
spuertor <spuertor@unal.edu.co>
2023-03-02 10:43:13 -0500

    BasicConfig

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,29 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : spuertor
+ * @brief          : BasicConfig
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+int main(void)
+{
+    /* Loop forever */
+	while(1);{
+
+	}
+
+	return 0;
+}
